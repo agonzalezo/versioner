f1cdbc14d805894caf3db8ce9cba58a4f8c83063
Alexander Gonzalez <mexi1415@gmail.com>
2023-09-05 23:08:32 -0500

    add os resources

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -12,7 +12,23 @@ router.get('/', (req, res) => {
         title: 'Versioner',
         version: '1.0.0',
         hostname: os.hostname(),
-        info: { arch: os.arch(), memory: os.totalmem(), uptime: os.uptime(), load: os.loadavg(), cpu: os.cpus() }
+        info:
+            [
+                {
+                    name: 'arch', value: os.arch()
+                },
+                {
+                    name: 'memory', value: os.totalmem()
+                },
+                {
+                    name: 'uptime', value: os.uptime()
+                },
+                {
+                    name: 'load', value: os.loadavg()
+                },
+                    { name: 'cpu', value: JSON.stringify(os.cpus()[0].model) }
+            ]
+        //        info: { arch: os.arch(), memory: os.totalmem(), uptime: os.uptime(), load: os.loadavg(), cpu: os.cpus() }
     })
 })
 

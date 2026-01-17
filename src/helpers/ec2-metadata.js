0e4e8407b2a8a457d94d52e1307f29440f526897
Alexander Gonzalez Osorio <agosori@bancolombia.com.co>
2026-01-17 00:14:03 -0500

    ommit http rules

--- src/helpers/ec2-metadata.js
+++ src/helpers/ec2-metadata.js
@@ -2,21 +2,25 @@ import axios from 'axios'
 
 const instanceData = {}
 
+// nosemgrep: typescript.react.security.react-insecure-request.react-insecure-request
 instanceData.getInstanceId = async () => {
   const { data } = await axios.get('http://169.254.169.254/latest/meta-data/instance-id')
   return data
 }
 
+// nosemgrep: typescript.react.security.react-insecure-request.react-insecure-request
 instanceData.getInstanceType = async () => {
   const { data } = await axios.get('http://169.254.169.254/latest/meta-data/instance-type')
   return data
 }
 
+// nosemgrep: typescript.react.security.react-insecure-request.react-insecure-request
 instanceData.getInstancePublicName = async () => {
   const { data } = await axios.get('http://169.254.169.254/latest/meta-data/public-hostname')
   return data
 }
 
+// nosemgrep: typescript.react.security.react-insecure-request.react-insecure-request
 instanceData.getInstancePublicIp = async () => {
   const { data } = await axios.get('http://169.254.169.254/latest/meta-data/public-ipv4')
   return data

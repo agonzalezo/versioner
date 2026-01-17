df8e67e46bbeff78624a915f46acd143b7a95ae9
Alexander Gonzalez Osorio <agosori@bancolombia.com.co>
2026-01-17 00:19:36 -0500

    ommit #2

--- src/helpers/ec2-metadata.js
+++ src/helpers/ec2-metadata.js
@@ -2,26 +2,26 @@ import axios from 'axios'
 
 const instanceData = {}
 
-// nosemgrep: typescript.react.security.react-insecure-request.react-insecure-request
 instanceData.getInstanceId = async () => {
+  // nosemgrep: typescript.react.security.react-insecure-request.react-insecure-request
   const { data } = await axios.get('http://169.254.169.254/latest/meta-data/instance-id')
   return data
 }
 
-// nosemgrep: typescript.react.security.react-insecure-request.react-insecure-request
 instanceData.getInstanceType = async () => {
+  // nosemgrep: typescript.react.security.react-insecure-request.react-insecure-request
   const { data } = await axios.get('http://169.254.169.254/latest/meta-data/instance-type')
   return data
 }
 
-// nosemgrep: typescript.react.security.react-insecure-request.react-insecure-request
 instanceData.getInstancePublicName = async () => {
+  // nosemgrep: typescript.react.security.react-insecure-request.react-insecure-request
   const { data } = await axios.get('http://169.254.169.254/latest/meta-data/public-hostname')
   return data
 }
 
-// nosemgrep: typescript.react.security.react-insecure-request.react-insecure-request
 instanceData.getInstancePublicIp = async () => {
+  // nosemgrep: typescript.react.security.react-insecure-request.react-insecure-request
   const { data } = await axios.get('http://169.254.169.254/latest/meta-data/public-ipv4')
   return data
 }
